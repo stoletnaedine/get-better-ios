5447bf6c646520b7b48100cb6349093c96dc70bc
Artur Islamgulov <artur.islamgulov@simbirsoft.com>
2020-04-24 15:47:40 +0300

    Добавил отображение Круга Жизни с тестовыми данными

--- GetBetter/Moduls/LifeCirclePage/LifeCircleController.swift
+++ GetBetter/Moduls/LifeCirclePage/LifeCircleController.swift
@@ -7,17 +7,57 @@
 //
 
 import UIKit
+import Charts
 
 class LifeCircleController: UIViewController {
 
     @IBOutlet weak var segmentedControl: UISegmentedControl!
-    @IBOutlet weak var circleView: UIView!
+    @IBOutlet weak var chartView: RadarChartView!
     @IBOutlet weak var detailsView: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.title = Properties.TabBar.lifeCircleTitle
         setupSegmentedControl()
+        setupChartView()
+    }
+    
+    func setupChartView() {
+        
+        // 2
+        chartView.webLineWidth = 1.5
+        chartView.innerWebLineWidth = 1.5
+        chartView.webColor = .lightGray
+        chartView.innerWebColor = .lightGray
+        
+        
+        chartView.rotationEnabled = false
+        chartView.legend.enabled = false
+        
+        let sphereValues = [6.0, 10.0, 4.0, 6.0, 9.0, 8.0, 6.0, 4.0]
+        let sphereValues2 = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+        
+        var dataEntries: [RadarChartDataEntry] = []
+        var dataEntries2: [RadarChartDataEntry] = []
+        
+        for i in 0..<sphereValues.count {
+            dataEntries.append(RadarChartDataEntry(value: sphereValues[i]))
+            dataEntries2.append(RadarChartDataEntry(value: sphereValues2[i]))
+        }
+        
+        let dataSet = RadarChartDataSet(entries: dataEntries, label: "Текущий уровень")
+        let dataSet2 = RadarChartDataSet(entries: dataEntries2, label: "Текущий уровень")
+        let data = RadarChartData(dataSets: [dataSet, dataSet2])
+        
+        dataSet.lineWidth = 3
+        
+        let redColor = UIColor(red: 247/255, green: 67/255, blue: 115/255, alpha: 1)
+        let redFillColor = UIColor(red: 247/255, green: 67/255, blue: 115/255, alpha: 0.6)
+        dataSet.colors = [redColor]
+        dataSet.fillColor = redFillColor
+        dataSet.drawFilledEnabled = true
+        
+        chartView.data = data
     }
     
     func setupSegmentedControl() {
@@ -29,10 +69,10 @@ class LifeCircleController: UIViewController {
     @IBAction func segmentedActionDidSelected(_ sender: UISegmentedControl) {
         switch sender.selectedSegmentIndex {
         case 0:
-            circleView.isHidden = false
+            chartView.isHidden = false
             detailsView.isHidden = true
         case 1:
-            circleView.isHidden = true
+            chartView.isHidden = true
             detailsView.isHidden = false
         default:
             print("default")
