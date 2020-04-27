f41bb12341951c5be04bbf544247eed023eb9576
Artur Islamgulov <artur.islamgulov@simbirsoft.com>
2020-04-27 11:32:41 +0300

    Fix some errors

--- GetBetter/Moduls/AboutAppPage/AboutAppViewController.swift
+++ GetBetter/Moduls/AboutAppPage/AboutAppViewController.swift
@@ -2,7 +2,7 @@
 //  WelcomeViewController.swift
 //  GetBetter
 //
-//  Created by Исламгулова Лариса on 23.04.2020.
+//  Created by Artur Islamgulov on 23.04.2020.
 //  Copyright © 2020 Artur Islamgulov. All rights reserved.
 //
 
