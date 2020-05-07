308dc060f0f9fe06b6a46f123a59ad25137c1850
Artur Islamgulov <artur.islamgulov@gmail.com>
2020-05-07 13:17:49 +0300

    Поправил верстку

--- GetBetter/Moduls/LifeCirclePage/AchievementsCell/AchievementsTableViewCell.swift
+++ GetBetter/Moduls/LifeCirclePage/AchievementsCell/AchievementsTableViewCell.swift
@@ -11,7 +11,7 @@ import UIKit
 class AchievementsTableViewCell: UITableViewCell {
 
     @IBOutlet weak var iconLabel: UILabel!
-    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var descriptionLabel: UILabel!
     
     override func awakeFromNib() {
@@ -24,15 +24,15 @@ class AchievementsTableViewCell: UITableViewCell {
     }
     
     func fillCell(from achievement: Achievement) {
-        self.nameLabel.text = achievement.title
+        self.titleLabel.text = achievement.title
         self.descriptionLabel.text = achievement.description
-        
     }
     
     private func setupView() {
         iconLabel.font = iconLabel.font.withSize(30)
-        nameLabel.font = UIFont.systemFont(ofSize: 22)
-        nameLabel.textColor = .darkGray
+        iconLabel.text = "🏆"
+        titleLabel.font = UIFont.systemFont(ofSize: 22)
+        titleLabel.textColor = .darkGray
         descriptionLabel.font = UIFont.systemFont(ofSize: 12)
         descriptionLabel.textColor = .gray
     }
