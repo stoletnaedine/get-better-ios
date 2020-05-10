6b009ca4109c64d796dfdf2dd7467f3c6362ce6e
Artur Islamgulov <artur.islamgulov@gmail.com>
2020-05-10 19:12:30 +0300

    Fill method in SphereDetailVC

--- GetBetter/Moduls/LifeCirclePage/SphereDetailPage/SphereDetailViewController.swift
+++ GetBetter/Moduls/LifeCirclePage/SphereDetailPage/SphereDetailViewController.swift
@@ -13,7 +13,6 @@ class SphereDetailViewController: UIViewController {
     
     @IBOutlet weak var textLabel: UILabel!
     @IBOutlet weak var sphereLabel: UILabel!
-    @IBOutlet weak var dateLabel: UILabel!
     @IBOutlet weak var photoImageView: UIImageView!
     @IBOutlet weak var cancelButton: UIButton!
     
@@ -47,45 +46,16 @@ class SphereDetailViewController: UIViewController {
     }
 
     func fillViewController(_ sphereValue: SphereValue) {
-        print("fillViewController FILL :)")
-//        self.title = post.text ?? Constants.Post.titleDefault
-//        self.textLabel.text = post.text ?? ""
-//        self.sphereLabel.text = post.sphere?.name ?? ""
-//        self.dateLabel.text = ""
-//        if let timestamp = post.timestamp {
-//            self.dateLabel.text = Date.convertToFullDate(from: timestamp)
-//        }
-//        DispatchQueue.global().async { [weak self] in
-//            if let urlString = post.photoUrl,
-//                let url = URL(string: urlString) {
-//
-//                DispatchQueue.main.async {
-//                    guard let selfView = self?.view else { return }
-//                    self?.showActivityIndicator(onView: selfView)
-//                }
-//
-//                if let imageData = try? Data(contentsOf: url),
-//                let image = UIImage(data: imageData) {
-//                    DispatchQueue.main.async {
-//                        self?.removeActivityIndicator()
-//                        self?.photoImageView.image = image
-//                        self?.photoImageView.isHidden = false
-//                    }
-//                } else {
-//                    DispatchQueue.main.async {
-//                        self?.removeActivityIndicator()
-//                    }
-//                }
-//            }
-//        }
+        sphereLabel.text = sphereValue.sphere?.name
+        if let value = sphereValue.value {
+            self.textLabel.text = "В этой сфере жизни вы прокачены на \(value) баллов."
+        }
     }
     
     func customizeView() {
         textLabel.font = UIFont.systemFont(ofSize: 18)
         sphereLabel.font = UIFont.boldSystemFont(ofSize: 24)
         sphereLabel.textColor = .violet
-        dateLabel.font = UIFont.systemFont(ofSize: 14)
-        dateLabel.textColor = .gray
         cancelButton.setTitle("", for: .normal)
         photoImageView.isHidden = true
     }
