2386765db6a25ff04d3aaef90e462074537d3195
Artur Islamgulov <islamgulov@technokratos.com>
2020-08-14 15:01:03 +0300

    Добавил отображение push, если приложение открыто на экране.

--- Classes/AppDelegate.swift
+++ Classes/AppDelegate.swift
@@ -18,37 +18,37 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
         FirebaseApp.configure()
+        setupNavigationBar()
+        Auth.auth().languageCode = "ru"
+        IQKeyboardManager.shared.enable = true
         
         if #available(iOS 10.0, *) {
-          // For iOS 10 display notification (sent via APNS)
-          UNUserNotificationCenter.current().delegate = self
+            UNUserNotificationCenter.current().delegate = self
 
-          let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
-          UNUserNotificationCenter.current().requestAuthorization(
-            options: authOptions,
-            completionHandler: {_, _ in })
+            let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
+            UNUserNotificationCenter.current().requestAuthorization(
+                options: authOptions,
+                completionHandler: {_, _ in }
+            )
         } else {
-          let settings: UIUserNotificationSettings =
-          UIUserNotificationSettings(types: [.alert, .badge, .sound], categories: nil)
-          application.registerUserNotificationSettings(settings)
+            let settings: UIUserNotificationSettings = UIUserNotificationSettings(
+                types: [.alert, .badge, .sound],
+                categories: nil
+            )
+            application.registerUserNotificationSettings(settings)
         }
 
         application.registerForRemoteNotifications()
         Messaging.messaging().delegate = self
-        
-        setupNavigationBar()
-        Auth.auth().languageCode = "ru"
-        IQKeyboardManager.shared.enable = true
         
         rootManager.start()
         
         return true
     }
     
-    func applicationWillTerminate(_ application: UIApplication) {
-    }
-    
-    func applicationDidFinishLaunching(_ application: UIApplication) {
+    // This method will be called when app received push notifications in foreground
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        completionHandler([.alert, .badge, .sound])
     }
     
     func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String) {
@@ -58,6 +58,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
       NotificationCenter.default.post(name: Notification.Name("FCMToken"), object: nil, userInfo: dataDict)
       // TODO: If necessary send token to application server.
       // Note: This callback is fired at each app startup and whenever a new token is generated.
+    }
+    
+    func applicationWillTerminate(_ application: UIApplication) {
+    }
+    
+    func applicationDidFinishLaunching(_ application: UIApplication) {
     }
     
     private func setupNavigationBar() {
