99cf5edafc81ec7ccf37da3815fbf3e6f6a4d2b5
Исламгулов Артур <islamgulov@technokratos.com>
2020-10-18 09:33:01 +0300

    Убрал отступы у textview в article

--- Classes/Screens/Article/ArticleViewController.swift
+++ Classes/Screens/Article/ArticleViewController.swift
@@ -12,7 +12,7 @@ class ArticleViewController: UIViewController {
 
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var textView: UITextView!
-    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var imageView: ScaledHeightImageView!
     
     var article: Article?
     
@@ -22,17 +22,24 @@ class ArticleViewController: UIViewController {
         guard let article = self.article else { return }
         fillViewController(from: article)
     }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        textView.contentInset = .zero
+        textView.textContainerInset = .zero
+        textView.textContainer.lineFragmentPadding = .zero
+        textView.isScrollEnabled = false
+    }
 
-    func fillViewController(from article: Article) {
+    private func fillViewController(from article: Article) {
         navigationItem.titleView = article.titleView
         titleLabel.text = article.title
         textView.attributedText = article.text?.htmlToAttributedString
         textView.sizeToFit()
-        imageView.image = article.image?.aspectFitImage(inRect: imageView.frame)
-        imageView.contentMode = .top
+        imageView.image = article.image
     }
     
-    func customizeView() {
+    private func customizeView() {
         titleLabel.font = UIFont.boldSystemFont(ofSize: 30)
         textView.textContainer.lineFragmentPadding = .zero
         textView.textContainerInset = .zero
