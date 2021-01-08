061c9264f7e115953dfcbea22bc1c54046698774
Исламгулов Артур <islamgulov@technokratos.com>
2021-01-08 16:51:59 +0300

    Рефакторинг NotificationService

--- Classes/Services/NotificationService.swift
+++ Classes/Services/NotificationService.swift
@@ -16,30 +16,33 @@ protocol NotificationService {
 class NotificationServiceDefault: NotificationService {
 
     private let alertService: AlertService = AlertServiceDefault()
-    private var settings: NotificationSettings?
     
     func subscribe(to settings: NotificationSettings) {
         // TODO: удалить когда все перейдут на версию 1.11
         Messaging.messaging().unsubscribe(fromTopic: "tipOfTheDay")
         Messaging.messaging().unsubscribe(fromTopic: "daily")
         
-        PostTopic.allCases.forEach({ topic in
-            Messaging.messaging().unsubscribe(fromTopic: topic.rawValue)
-        })
-        Messaging.messaging().subscribe(toTopic: settings.post.rawValue) { [weak self] error in
-            if let error = error {
-                self?.alertService.showErrorMessage(desc: error.localizedDescription)
-                return
-            }
-        }
-        
         TipTopic.allCases.forEach({ topic in
             Messaging.messaging().unsubscribe(fromTopic: topic.rawValue)
         })
-        Messaging.messaging().subscribe(toTopic: settings.tip.rawValue) { [weak self] error in
-            if let error = error {
-                self?.alertService.showErrorMessage(desc: error.localizedDescription)
-                return
+        if settings.tip != .none {
+            Messaging.messaging().subscribe(toTopic: settings.tip.rawValue) { [weak self] error in
+                if let error = error {
+                    self?.alertService.showErrorMessage(desc: error.localizedDescription)
+                    return
+                }
+            }
+        }
+            
+        PostTopic.allCases.forEach({ topic in
+            Messaging.messaging().unsubscribe(fromTopic: topic.rawValue)
+        })
+        if settings.post != .none {
+            Messaging.messaging().subscribe(toTopic: settings.post.rawValue) { [weak self] error in
+                if let error = error {
+                    self?.alertService.showErrorMessage(desc: error.localizedDescription)
+                    return
+                }
             }
         }
         
